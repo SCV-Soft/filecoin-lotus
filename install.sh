0ee94ee7413a9ef1ac6424578d7ee32f18d66453
benjioh5 <benjioh5@gmail.com>
2021-03-16 17:43:14 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# apt install packages
+sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
+
+# rustup + golang
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+wget -c https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+
 mkdir /tmp/lotus-tmp
 cd /tmp/lotus-tmp
-curl 
-tar -zxvf lotus-1.5.2.tar.gz
+curl https://github.com/SCV-Soft/filecoin-lotus/releases/download/1.5.2/lotus.tar.gz
+tar -zxvf lotus.tar.gz
 sudo install -C ./lotus /usr/local/bin/lotus
 sudo install -C ./lotus-miner /usr/local/bin/lotus-miner
 sudo install -C ./lotus-worker /usr/local/bin/lotus-worker
